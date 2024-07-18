42429a190f585f23bd515a218a39e1ff662afd05
artemsabunin <shuba.2001@inbox.ru>
2024-07-18 13:30:18 +0300

    fix

--- app/src/main/java/com/example/simple_diary_planner/feature_create_task/presentation/CreateTaskScreen.kt
+++ app/src/main/java/com/example/simple_diary_planner/feature_create_task/presentation/CreateTaskScreen.kt
@@ -95,14 +95,6 @@ class CreateTaskScreen : BaseScreen<CreateTaskViewModel>() {
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            val timeSlots = listOf(
-                "00:00-01:00", "01:00-02:00", "02:00-03:00", "03:00-04:00",
-                "04:00-05:00", "05:00-06:00", "06:00-07:00", "07:00-08:00",
-                "08:00-09:00", "09:00-10:00", "10:00-11:00", "11:00-12:00",
-                "12:00-13:00", "13:00-14:00", "14:00-15:00", "15:00-16:00",
-                "16:00-17:00", "17:00-18:00", "18:00-19:00", "19:00-20:00",
-                "20:00-21:00", "21:00-22:00", "22:00-23:00", "23:00-00:00"
-            )
             ExposedDropdownMenuBox(
                 expanded = dropdownExpanded,
                 onExpandedChange = { dropdownExpanded = !dropdownExpanded },
